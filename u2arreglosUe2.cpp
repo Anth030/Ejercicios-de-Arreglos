93f8aea669c2441289e976d286c40e19c85d88c8
Anth030 <ajimenezca@unjbg.edu.pe>
2024-08-06 23:24:51 +0200

    programa que hace eco

---
+++ u2arreglosUe2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	char* sonido[100];
+	char palabra[100];
+	cout<<"diga algo..."<<endl;
+	while(palabra!=false){
+	for(int i=0;i<1;i++){
+		gets(palabra);
+		sonido[i]=palabra;
+		cout<<sonido[2]<<endl;
+	}
+	}
+	return 0;
+}
